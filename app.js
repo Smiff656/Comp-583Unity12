ea0f66838f0d76d3530f75fae236269ac54474ec
Glitch (glitch-hello-website) <none>
2023-05-13 04:13:17 +0000

    👩🗾 Checkpoint ./index.html:48254069/23041 ./style.css:48254069/16893 ./app.js:48254069/1113

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
+
+
+
+registerLink.addEventListener('click', ()=> {wrapper.classList.add('active');
+});
+loginLink.addEventListener('click', ()=> {wrapper.classList.remove('active');
+});
+btnPopup.addEventListener('click', ()=> {wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {wrapper.classList.remove('active-popup');
 });
